414fb836cdaa63aa94bb2875fe2b006cd337710c
Illia Rahavoi <irahavoi@gmail.com>
2016-03-05 22:39:37 -0600

    Just some notes

---
+++ out/production/javaWebServices/com/irahavoi/jaxws/GetTimeAsString.java
@@ -0,0 +1,15 @@
+
+package com.irahavoi.jaxws;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement(name = "getTimeAsString", namespace = "http://irahavoi.com/")
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "getTimeAsString", namespace = "http://irahavoi.com/")
+public class GetTimeAsString {
+
+
+}
---
+++ src/com/irahavoi/jaxws/GetTimeAsElapsed.java
@@ -0,0 +1,15 @@
+
+package com.irahavoi.jaxws;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement(name = "getTimeAsElapsed", namespace = "http://irahavoi.com/")
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "getTimeAsElapsed", namespace = "http://irahavoi.com/")
+public class GetTimeAsElapsed {
+
+
+}
